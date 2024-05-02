1d597f81cd03436b368719d4f35d3a49ce82b50f
AntonKinnander <a.kinnander@icloud.com>
2024-05-02 23:46:31 +0200

    gamestates

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
+let gamestate = "start";
+
 function setup() {
   rectMode(CENTER);
 }
 function draw() {
-  const w = width / 12;
-  const h = width / 8;
-  for (let i = 0; i < 3; i++) {
-    const y = 0 + (h + 25) * i;
-    for (let i = 0; i < 5; i++) {
-      const x = 0 + (w + 25) * i;
+  background(0);
+  switch (gamestate) {
+    case "start":
       translate(width / 4, height / 4);
-      createClickArea(x, y, w, h);
-    }
+      if (createClickArea(width / 4, height / 4, 200, 75)) {
+        gamestate = "game";
+      }
+      break;
+    case "game":
+      translate(width / 4, height / 4);
+      drawHitBoxes();
+      break;
   }
 }
 
 // function buttonCoordinates(){
 
 // }
+
+function drawHitBoxes() {
+  const w = width / 12;
+  const h = width / 8;
+  for (let i = 0; i < 3; i++) {
+    const y = 0 + (h + 25) * i;
+    for (let i = 0; i < 5; i++) {
+      const x = 0 + (w + 25) * i;
+
+      createClickArea(x, y, w, h);
+    }
+  }
+}
 
 function createClickArea(x, y, w, h) {
   //green default
@@ -33,6 +51,7 @@ function createClickArea(x, y, w, h) {
     //red if user clicks
     fill(255, 0, 0);
     console.log("mega kill!!!");
+    return true;
   }
   drawTestRectangle(x, y, w, h);
 }
